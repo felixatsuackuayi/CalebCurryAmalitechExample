e541a12c43a9213e2c53f885ab9fd02e5b15434d
Felix Ackuayi <fackuayi@payswitch.com.gh>
2020-09-24 17:05:48 +0000

    add input in arrays

---
+++ MySweetProgram/src/reverseArray.java
@@ -0,0 +1,13 @@
+public class reverseArray
+{
+    public static void main(String[] args){
+        int [] reversal = {11,42,-5,27,0,89};
+
+        for (int i = reversal.length - 1; i >= 0; i--) {
+
+            System.out.println(reversal[i]);
+
+        }
+
+    }
+}
